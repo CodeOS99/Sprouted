4144b26afcce377bdab5f5ed811776a0c5cdf0c4
CodeOS99 <kush.kuhu@gmail.com>
2026-03-08 19:48:31 +0530

    Attempted to add an idle bobbing animation, but i felt it doesnt look too good, so i removed it. Also fixes some bugs.

--- scripts/popout_log.gd
+++ scripts/popout_log.gd
@@ -1,11 +1,27 @@
 extends RigidBody3D
 
-const horizontal_force: float = 50.0
-const vertical_force: float = 50.0
+const HORIZONTAL_FORCE: float = 50.0
+const VERTICAL_FORCE: float = 50.0
+
+var base_position: Vector3
+var time := 0.0
+var landed := false
 
 func _ready() -> void:
 	var dir = Vector3(randf_range(-0.5,0.5), 0, randf_range(-0.5,0.5)).normalized()
-	self.apply_impulse(dir * horizontal_force + Vector3.UP * vertical_force)
-
-func _process(delta: float) -> void:
-	pass
+	apply_impulse(dir * HORIZONTAL_FORCE + Vector3.UP * VERTICAL_FORCE)
+#
+#func _physics_process(delta: float) -> void:
+	#if not landed and get_contact_count() > 0:
+		#start_idle()
+#
+#func start_idle():
+	#landed = true
+	#freeze = true
+	#base_position = global_position
+	#set_process(true)
+#
+#func _process(delta: float) -> void:
+	#if landed:
+		#time += delta
+		#global_position.y = base_position.y + sin(time * 2.0) * 0.1
