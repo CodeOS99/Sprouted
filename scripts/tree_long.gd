e67a9cc4df360eef1c1be59c409824d822105bde
CodeOS99 <kush.kuhu@gmail.com>
2026-03-29 19:10:33 +0530

    The tree has a 50% change to spawn a skeleton for each log that is given

--- scripts/tree_long.gd
+++ scripts/tree_long.gd
@@ -1,9 +1,8 @@
 extends StaticBody3D
 
 @onready var mesh = $"mesh"
-
 var popout_log = preload("res://scenes/popout_log.tscn")
-
+var skeleton_minion = preload("res://scenes/skeleton_minion.tscn")
 const MAX_HEALTH: float = 20.0
 var health: int = MAX_HEALTH
 
@@ -12,16 +11,21 @@ func spawn_log():
 	get_tree().root.add_child(log)
 	log.global_position = $LogPoint.global_position
 
-func chopped(damage: float) -> bool: # also return whether its chopped or not
-	for i in range(health-damage, health): # spawn a log every fourth damage
+func spawn_skeleton():
+	var skeleton := skeleton_minion.instantiate()
+	get_tree().root.add_child(skeleton)
+	skeleton.global_position = $LogPoint.global_position
+
+func chopped(damage: float) -> bool:
+	for i in range(health-damage, health):
 		if i % 4 == 0:
 			$Pop.play()
 			self.spawn_log()
-
+			if randi() % 2 == 0: # 50% chance per log
+				spawn_skeleton()
 	health -= damage
-	self.mesh.scale.x-=(damage/MAX_HEALTH*4.0)
-	self.mesh.scale.y-=(damage/MAX_HEALTH*4.0)
-	self.mesh.scale.z-=(damage/MAX_HEALTH*4.0)
+	self.mesh.scale.x -= (damage / MAX_HEALTH * 4.0)
+	self.mesh.scale.y -= (damage / MAX_HEALTH * 4.0)
+	self.mesh.scale.z -= (damage / MAX_HEALTH * 4.0)
 	Globals.player.shake_camera()
-	
 	return self.mesh.scale.x <= 0
